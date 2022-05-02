74fdbe7636b26eaf953b8cf9ec4db98db1facd58
Joker-40 <70046537+Joker-40@users.noreply.github.com>
2022-05-02 15:50:08 +0530

    first

--- ang-ex/src/app/app.module.ts
+++ ang-ex/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NavbarComponent } from './sharedPages/navbar/navbar.component';
+import { FooterComponent } from './sharedPages/footer/footer.component';
+import { TableComponent } from './sharedPages/table/table.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    NavbarComponent,
+    FooterComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ang-ex/src/app/sharedPages/table/table.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormArray, FormGroup, FormControl } from '@angular/forms';
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  info = this.fb.group({
+    infoData: this.fb.array([])
+  })
+
+  get infoData() : FormArray{
+     return this.info.controls["infoData"] as FormArray;
+  }
+  
+  addData() {
+    const infoDataForm = this.fb.group({
+      fname : ["", Validators.required],
+      lname: ["", Validators.required],
+      age:["",Validators.required] 
+    })
+    this.infoData.push(infoDataForm);
+  }
+
+  removeInfo(i:number) {
+    this.infoData.removeAt(i);
+  }
+}
